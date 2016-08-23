e0d9a130c598d57bdad6d7808112ec4975b47d70
sergiomrico1 <sergio.martinezr.external@atos.net>
2016-08-23 12:40:03 +0200

    Actualización de Javadocs y nuevas funciones para el API (contar número de páginas y añadir campo de firma vacío a un pdf)

--- afirma-keyone/src/main/java/es/gob/afirma/keyone/KeyOneUtil.java
+++ afirma-keyone/src/main/java/es/gob/afirma/keyone/KeyOneUtil.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore.PrivateKeyEntry;
 import java.security.KeyStoreException;
@@ -11,13 +12,19 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableEntryException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
 
+import com.aowagie.text.DocumentException;
+import com.aowagie.text.Rectangle;
+import com.aowagie.text.pdf.PdfAnnotation;
+import com.aowagie.text.pdf.PdfFormField;
+import com.aowagie.text.pdf.PdfName;
 import com.aowagie.text.pdf.PdfReader;
 import com.aowagie.text.pdf.PdfStamper;
-
+import com.aowagie.text.pdf.PdfString;
 import es.gob.afirma.cert.signvalidation.SignValiderFactory;
 import es.gob.afirma.cert.signvalidation.SignValidity.SIGN_DETAIL_TYPE;
 import es.gob.afirma.core.AOCancelledOperationException;
@@ -42,11 +49,19 @@ import es.gob.afirma.signers.pades.PdfUtil.SignatureField;
 import es.gob.afirma.standalone.SimpleKeyStoreManager;
 import es.gob.afirma.standalone.ui.preferences.PreferencesManager;
 
+/** API con funcionalidades básicas sobre documentos PDF.
+ * @author Sergio Mart&iacute;nez Rico
+ */
 public final class KeyOneUtil {
 
 	private static final String SEPARATOR = ","; //$NON-NLS-1$
 	private static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
 
+	/** Obtiene los nombres de los campos de firma de un documento pdf.
+	 * @param filePath Ruta del documento pdf.
+	 * @return Nombres de los campos de firma de un documento pdf.
+	 * @throws PdfException Error al gestionar el documento pdf.
+	 */
 	public static String enumSignatureFieldNames(final String filePath) throws PdfException {
 		final StringBuilder sb = new StringBuilder();
         try ( final InputStream fis = new FileInputStream(new File(filePath)) ) {
@@ -64,15 +79,37 @@ public final class KeyOneUtil {
         }
 	}
 
+	/** Obtiene el n&uacute;mero de p&aacute;ginas del documento pdf.
+	 * @param filePath Ruta del documento pdf.
+	 * @return N&uacute;mero de p&aacute;ginas del documento pdf.
+	 * @throws PdfException Error al gestionar el documento pdf.
+	 */
+	public static int getPdfPageNumber(final String filePath) throws PdfException {
+        PdfReader pdfReader;
+		try {
+			pdfReader = new PdfReader(filePath);
+		} catch (IOException e) {
+			LOGGER.severe("Error anadiendo pagina en blanco al PDF: " + e); //$NON-NLS-1$
+			throw new PdfException("Error obteniendo el n&uacute;mero de p&aacute;ginas del documento PDF: " + e, e); //$NON-NLS-1$
+		}
+        return pdfReader.getNumberOfPages();
+	}
+	
+	/** Agrega una p&aacute;gina en blanco al final del documento pdf.
+	 * @param filePath Ruta del fichero pdf.
+	 * @throws PdfException Error al gestionar el fichero pdf.
+	 */
 	public static void addBlankPage(final String filePath) throws PdfException {
-        try ( final ByteArrayOutputStream baos = new ByteArrayOutputStream() ) {
+        try ( 
+        		final ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
+        		final FileOutputStream os = new FileOutputStream(new File(filePath))
+        				) {
         	final PdfReader pdfReader = new PdfReader(filePath);
         	final Calendar cal = Calendar.getInstance();
         	final PdfStamper stp = new PdfStamper(pdfReader, baos, cal);
         	stp.insertPage(pdfReader.getNumberOfPages() + 1, pdfReader.getPageSizeWithRotation(1));
         	stp.close(cal);
         	pdfReader.close();
-        	final FileOutputStream os = new FileOutputStream(new File(filePath));
         	os.write(baos.toByteArray());
         	os.close();
         }
@@ -82,6 +119,20 @@ public final class KeyOneUtil {
         }
 	}
 
+	/** Firma del fichero pdf.
+	 * @param originalPath Ruta de origen.
+	 * @param destinyPath Ruta de destino.
+	 * @param policyIdentifier Identificador de pol&iacute;ticas.
+	 * @param fieldName Nombre del campo.
+	 * @param tsaName Nombre tsa.
+	 * @param xmlLook Apariencia del XML.
+	 * @throws PdfException Error en la lectua del pdf.
+	 * @throws XMLException Errir en la lectura del XML.
+	 * @throws AOCertificatesNotFoundException No se han encontrado certficados.
+	 * @throws BadPdfPasswordException Password incorrecta.
+	 * @throws PdfIsCertifiedException Pdf no certificado.
+	 * @throws PdfHasUnregisteredSignaturesException Firma del pdf no registrada.
+	 */
 	public static void pdfSign(final String originalPath,
 								   final String destinyPath,
 								   final String policyIdentifier,
@@ -135,11 +186,11 @@ public final class KeyOneUtil {
             pke = getPrivateKeyEntry(filters);
         }
         catch (final AOCancelledOperationException e) {
-        	throw new AOCancelledOperationException("Cancelado por el usuario: " + e, e); //$NON-NLS-1$
+        	throw e; 
         }
         catch(final AOCertificatesNotFoundException e) {
         	LOGGER.severe("El almacen no contiene ningun certificado que se pueda usar para firmar: " + e); //$NON-NLS-1$
-        	throw new AOCertificatesNotFoundException("El almacen no contiene ningun certificado que se pueda usar para firmar: " + e); //$NON-NLS-1$
+        	throw e;
         }
         catch (final Exception e) {
         	LOGGER.severe("Ocurrio un error al extraer la clave privada del certificiado seleccionado: " + e); //$NON-NLS-1$
@@ -153,7 +204,9 @@ public final class KeyOneUtil {
         new XMLLookParser(xmlLook, field, p, pke).parse();
 
         final byte[] signResult;
-        try {
+        try (
+        		final FileOutputStream os = new FileOutputStream(new File(destinyPath))
+        		){
             signResult = signer.sign(
         		data,
         		signatureAlgorithm,
@@ -161,7 +214,6 @@ public final class KeyOneUtil {
                 pke.getCertificateChain(),
                 p
             );
-            final FileOutputStream os = new FileOutputStream(new File(destinyPath));
         	os.write(signResult);
         	os.close();
         }
@@ -170,15 +222,15 @@ public final class KeyOneUtil {
         }
         catch(final PdfIsCertifiedException e) {
         	LOGGER.severe("PDF no firmado por estar certificado: " + e); //$NON-NLS-1$
-        	throw new PdfIsCertifiedException();
+        	throw e;
         }
         catch(final BadPdfPasswordException e) {
         	LOGGER.severe("PDF protegido con contrasena mal proporcionada: " + e); //$NON-NLS-1$
-        	throw new BadPdfPasswordException(e);
+        	throw e;
         }
         catch(final PdfHasUnregisteredSignaturesException e) {
         	LOGGER.severe("PDF con firmas no registradas: " + e); //$NON-NLS-1$
-        	throw new PdfHasUnregisteredSignaturesException();
+        	throw e;
         }
         catch(final OutOfMemoryError ooe) {
             LOGGER.severe("Falta de memoria en el proceso de firma: " + ooe); //$NON-NLS-1$
@@ -190,6 +242,11 @@ public final class KeyOneUtil {
         }
 	}
 
+	/** Verifica la firma correcta de un fichero.
+	 * @param filePath Ruta del fichero firmado.
+	 * @return True si la verificaci&oacute;n es correcta.
+	 * @throws PdfException Error en la apertura o en el acceso al fichero.
+	 */
 	public static boolean verifySignature(final String filePath) throws PdfException {
 		byte[] sign = null;
 		try ( final FileInputStream fis = new FileInputStream(new File(filePath)) ) {
@@ -202,11 +259,16 @@ public final class KeyOneUtil {
 		}
 	}
 
-	public static PrivateKeyEntry getPrivateKeyEntry(final List<? extends CertificateFilter> filters) throws UnrecoverableEntryException,
-																											  AOKeyStoreManagerException,
-																											  AOCertificatesNotFoundException,
-																											  KeyStoreException,
-																											  NoSuchAlgorithmException
+	/** Obtiene la clave privada tras aplicar los filtros.
+	 * @param filters Filtros para la obtenci&oacute;n de la clave.
+	 * @return Clave privada.
+	 * @throws UnrecoverableEntryException Error en la inicializaci&oacute;n del almac&eacute;n.
+	 * @throws AOCertificatesNotFoundException No se han encontrados certificados.
+	 * @throws AOKeyStoreManagerException Error en la inicializaci&oacute;n del almac&eacute;n.
+	 * @throws KeyStoreException Error en la inicializaci&oacute;n del almac&eacute;n.
+	 * @throws NoSuchAlgorithmException No existe el algoritmo.
+	 */
+	public static PrivateKeyEntry getPrivateKeyEntry(final List<? extends CertificateFilter> filters) throws UnrecoverableEntryException, AOCertificatesNotFoundException, AOKeyStoreManagerException, KeyStoreException, NoSuchAlgorithmException
 	{
 		final AOKeyStoreManager ksm = SimpleKeyStoreManager.getKeyStore(false, null);
 		final AOKeyStoreDialog dialog = new AOKeyStoreDialog(
@@ -225,7 +287,60 @@ public final class KeyOneUtil {
 		);
 	}
 
-	public static String cnTarjeta() throws SmartCardException{
+	/** Se crea un campo vac&iacute;o de firma para una p&aacute;gina y posici&oacute;n concretos.
+	 *  La p&aacute;gina puede ser menor a 0 para comenzar a contar desde la &uacute;ltima p&aacute;gina
+	 *  (-1 = &uacute;ltima, -2 = pen&uacute;ltima, etc). 
+	 *  En caso de ser 0 el campo se crea en la primera p&aacute;gina.
+	 * @param filePath Fichero pdf sobre el que incluir el campo de firma.
+	 * @param page N&uacute;mero de p&aacute;gina.
+	 * @param leftX Coordinada izquierda X de la posici&oacute;n del campo.
+	 * @param leftY Coordinada izquierda Y de la posici&oacute;n del campo.
+	 * @param rightX Coordinada derecha X de la posici&oacute;n del campo.
+	 * @param rightY Coordinada izquierda Y de la posici&oacute;n del campo.
+	 * @throws DocumentException Error al abrir el documento.
+	 * @throws IOException Error al encontrar el fichero.
+	 * @throws PdfException Error al obtener el n&uacute;mero de p&aacute;ginas.
+	 * @throws IllegalArgumentException N&uacute;mero de p&aacute;gina fuera de los l&iacute;mites.
+	 */
+	public static void addSignField(final String filePath, final int page, final int leftX, final int leftY, final int rightX, final int rightY) throws DocumentException, IOException, PdfException {
+		int pageNbr = getPdfPageNumber(filePath);
+		if(page > pageNbr || page < pageNbr*-1) {
+			throw new IllegalArgumentException("El numero de pagina no puede ser superior al numero total"); //$NON-NLS-1$
+		}
+		final PdfReader reader = new PdfReader(filePath);
+		try (
+				FileOutputStream fos = new FileOutputStream(filePath)
+				) {
+			PdfStamper stamper = new PdfStamper(reader, fos, new GregorianCalendar());
+			PdfFormField sig = PdfFormField.createSignature(stamper.getWriter()); 
+			sig.setWidget(new Rectangle(leftX, leftY, rightX, rightY), null); 
+			sig.setFlags(PdfAnnotation.FLAGS_PRINT); 
+			sig.put(PdfName.DA, new PdfString("/Helv 0 Tf 0 g"));  //$NON-NLS-1$
+			sig.setFieldName("SIGNATURE");  //$NON-NLS-1$
+			int finalPage;
+			if(page > 0) {
+				finalPage = page;
+			}
+			else if(page < 0) {
+				finalPage = pageNbr + page + 1;
+			}
+			else {
+				finalPage = 1;
+			}
+			sig.setPage(finalPage); 
+			stamper.addAnnotation(sig, finalPage); 
+			stamper.close(new GregorianCalendar()); 
+		}
+		catch (IOException e) {
+			throw new IOException("El fichero " + filePath + " no existe: " + e); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+	
+	/** Obtiene el CN del certificado cargado de una tarjeta.
+	 * @return CN de la tarjeta.
+	 * @throws SmartCardException Error en la inicializaci&oacute;n de la tarjeta
+	 */
+	public static String cnTarjeta() throws SmartCardException {
 		AOKeyStoreManager ksm = null;
 		try {
 			ksm = AOKeyStoreManagerFactory.getAOKeyStoreManager(
